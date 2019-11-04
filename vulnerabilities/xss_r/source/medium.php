c8b20b799f018a3e67d376d025fc9939375b9b90
Arun Pillai <arun.pillai6@wipro.com>
2019-11-04 17:18:41 +0000

    uploaded with impossible fix

--- vulnerabilities/xss_r/source/medium.php
+++ vulnerabilities/xss_r/source/medium.php
@@ -4,8 +4,11 @@ header ("X-XSS-Protection: 0");
 
 // Is there any input?
 if( array_key_exists( "name", $_GET ) && $_GET[ 'name' ] != NULL ) {
+	// Get input
+	$name = str_replace( '<script>', '', $_GET[ 'name' ] );
+
 	// Feedback for end user
-	$html .= '<pre>Hello ' . $_GET[ 'name' ] . '</pre>';
+	$html .= "<pre>Hello ${name}</pre>";
 }
 
 ?>
